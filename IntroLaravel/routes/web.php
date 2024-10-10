009a998cdc084e4f826c2da2ad2ede99048d2c0a
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2024-10-09 23:14:53 -0600

    controlador1

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+Route::get('/',[controladorVistas::class, 'home'])->name('rutainicio');
+Route::get('/form', [controladorVistas::class, 'formulario'])->name('rutaformulario');
+Route::get('/clientes', [controladorVistas::class, 'consulta'])->name('rutaclientes');
+Route::view('/componentes','componentes')->name(name: 'rutacomponentes');
+
+
+
 //ruta de tipo GET
 /*Route::get('/', function () {
     return view('welcome');
 });*/
 
-//ruta de tipo view
+/* //ruta de tipo view
 Route::view('/',view: 'inicio')->name('rutainicio');
 Route::view('/form','formulario')->name(name: 'rutaformulario');
 Route::view('/clientes','clientes')->name(name: 'rutaclientes');
-Route::view('/componentes','componentes')->name(name: 'rutacomponentes');
+ */
