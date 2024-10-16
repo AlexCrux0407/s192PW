af250fa80148cc7847b855a883c46d92950425fe
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2024-10-16 11:44:57 -0600

    Logica del convertidor

--- prácticasRepasos/app/Http/Controllers/ControladorRepaso.php
+++ prácticasRepasos/app/Http/Controllers/ControladorRepaso.php
@@ -10,4 +10,35 @@ class ControladorRepaso extends Controller
     {
         return view('repaso1');
     }
+
+    public function convertidor(Request $request)
+    {
+        $cantidad = $request->input('cantidad');
+        $unidad = $request->input('unidad');
+        $convertida = $request->input('convertida');
+
+        $result = $this->convertirUnidades($cantidad, $unidad, $convertida);
+
+        return view('repaso1', ['result' => $result]);
+    }
+
+    private function convertirUnidades($cantidad, $unidad, $convertida)
+        {
+            $unidades = [
+                'MB' => 1,
+                'GB'=> 1024,
+                'TB' => 1024*1024   
+            ];
+            if(!isset($unidades[$unidad]) || !isset($unidades[$convertida]))
+            {
+                return "Unidad inválida";
+            }
+            $cantidadMB=$cantidad * $unidades[@unidad];
+
+            $cantidad_convertida = $cantidadMB / $unidades[@unidad];
+
+            return '{$cantidad} {$unidad} equivalen a {$cantidad_convertida} {$convertida}.';
+        }
+        
 }
+
